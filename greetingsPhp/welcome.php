65cdb6be8d69b42b1a4c72f3edd682758fa7b197
Fagner <fagnergeo0@gmail.com>
2024-08-01 22:13:31 -0300

    Implementei um formulário onde pode inserir nome e email e receber as boas vindas ao meu primeiro código php. Usando xampp, transformei minha máquina em um servidor com apache e fiz uma requisição usando o método get dentro do arquivo welcome.php que esta sendo referenciado no client side form.html mostrando nome e email. - I implemented a form that possibilities to insert name and email and get greetings to my first php code. Using xampp, I transformed my machine into a server with apache and did a request using the get method inside of the welcome.php file which has being referenced on the form.html client side showing name and email.

---
+++ greetingsPhp/welcome.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bem vindo php!</title>
+</head>
+<body>
+    <p>Bem bindo: <?php echo $_GET["name"] ?></p>
+    <p>Seu endereço de email é: <?php echo $_GET["email"] ?></p>
+</body>
+</html>
