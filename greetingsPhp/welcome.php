56a414d2243c2f3c2bdd5c62252c7b5be084f439
Fagner <fagnergeo0@gmail.com>
2024-08-13 20:39:42 -0300

    updating

--- greetingsPhp/welcome.php
+++ greetingsPhp/welcome.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bem vindo php!</title>
+
+    <link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
     <p>Bem bindo: <?php echo $_GET["name"] ?></p>
     <p>Seu endereço de email é: <?php echo $_GET["email"] ?></p>
+
+    <div class="text-box">
+            <a href="form.html" class="btn btn-white btn-animate">Back to form</a>
+    </div>
 </body>
 </html>
